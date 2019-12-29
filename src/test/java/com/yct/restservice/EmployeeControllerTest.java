cb0da1212a0d2864237403ec94a30ae23d189339
yachingtang2 <yachingtang@gmail.com>
2019-12-29 14:48:43 -0600

    Remove class level public access modifier.

--- src/test/java/com/yct/restservice/EmployeeControllerTest.java
+++ src/test/java/com/yct/restservice/EmployeeControllerTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class EmployeeControllerTest {
+class EmployeeControllerTest {
 
   @Autowired
   private MockMvc mvc;
